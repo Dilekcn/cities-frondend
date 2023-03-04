d8f4563f8c08f0671ef7210c2f373de613c69a44
Dilekcn <canturkdk@gmail.com>
2023-03-04 00:49:17 +0000

    css added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const citydesc = document.querySelector("#data");
+
+fetch("http://localhost:5000/")
+  .then((res) => res.json())
+  .then((data) => {
+    citydesc.innerText = data;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
