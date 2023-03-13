475e2ce92770c8b1236115747f0b3877a94da467
Dilekcn <canturkdk@gmail.com>
2023-03-13 12:03:33 +0000

    ec2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const citydesc = document.querySelector("#data");
 
-fetch("http://localhost:5000/")
+fetch("http://52.91.135.178:5000")
   .then((res) => res.json())
   .then((data) => {
     citydesc.innerText = data;
